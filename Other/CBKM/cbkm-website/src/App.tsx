094921c5fd7aefa64edd9e48f0a222abfbd922a1
singu0380 <singu0380@gmail.com>
2026-05-05 16:21:12 -0600

    Change Router from BrowserRouter to HashRouter

--- Other/CBKM/cbkm-website/src/App.tsx
+++ Other/CBKM/cbkm-website/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { SponsorshipProvider } from './context/SponsorshipContext';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
